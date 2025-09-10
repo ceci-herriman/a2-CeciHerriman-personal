a4e71220447774793ab719a8a4c9c10101586c9e
ceciherriman <herrimanceci@gmail.com>
2025-09-10 09:28:58 -0400

    frontend and backend changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,18 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
 
+let currentlyEditingOldData = null;
+
 const submit = async function( event ) {
-  // stop form submission from trying to load
-  // a new .html page for displaying results...
-  // this was the original browser behavior and still
-  // remains to this day
+  clearEditing()
+
   event.preventDefault()
-  
-  const input = document.querySelector( "#yourname" ),
-        json = { yourname: input.value },
+
+  const name = document.querySelector( "#name" ),
+        year = document.querySelector( "#year" ),
+        plotRating = document.querySelector( "#plotRating" ),
+        actingRating = document.querySelector( "#actingRating" ),
+        musicRating = document.querySelector( "#musicRating" ),
+        json = { name: name.value, year: year.value, plotRating: plotRating.value, actingRating: actingRating.value, musicRating: musicRating.value },
         body = JSON.stringify( json )
 
   const response = await fetch( "/submit", {
@@ -16,12 +20,208 @@ const submit = async function( event ) {
     body 
   })
 
-  const text = await response.text()
+  const data = await response.json()
 
-  console.log( "text:", text )
+  console.log( "text:", data )
+
+  //clear inputs
+  name.value = ''
+  year.value = ''
+  plotRating.value = ''
+  actingRating.value = ''
+  musicRating.value = ''
+
+  populateTable(data)
+}
+
+const deleteRow = async function( rowIndex ) {
+  clearEditing()
+
+  console.log("Delete row")
+
+  const name = document.getElementById( `name-${rowIndex}` ),
+        year = document.getElementById( `year-${rowIndex}` ),
+        plotRating = document.getElementById( `plotRating-${rowIndex}` ),
+        actingRating = document.getElementById( `actingRating-${rowIndex}` ),
+        musicRating = document.getElementById( `musicRating-${rowIndex}` ),
+        json = { name: name.textContent, 
+          year: year.textContent, 
+          plotRating: plotRating.textContent, 
+          actingRating: actingRating.textContent, 
+          musicRating: musicRating.textContent },
+        body = JSON.stringify( json )
+
+  console.log("deleting: ", body)
+
+  const response = await fetch( "/", {
+    method:"DELETE",
+    body 
+  })
+
+  const data = await response.json()
+
+  console.log( "text:", data )
+
+  populateTable(data)
+}
+
+const editRow = function( rowIndex ) {
+
+  const name = document.getElementById( `name-${rowIndex}` ),
+        year = document.getElementById( `year-${rowIndex}` ),
+        plotRating = document.getElementById( `plotRating-${rowIndex}` ),
+        actingRating = document.getElementById( `actingRating-${rowIndex}` ),
+        musicRating = document.getElementById( `musicRating-${rowIndex}` ),
+        plotRatingOldVal = plotRating.textContent,
+        actingRatingOldVal = actingRating.textContent,
+        musicRatingOldVal = musicRating.textContent
+
+  //populate edit fields
+  let editName = document.getElementById('edit-name');
+  editName.textContent = name.textContent
+
+  let editYear = document.getElementById('edit-year');
+  editYear.textContent = year.textContent
+
+  let plotInputField = document.getElementById('edit-plot');
+  plotInputField.value = plotRatingOldVal;
+
+  let actingInputField = document.getElementById('edit-acting');
+  actingInputField.value = actingRatingOldVal;
+
+  let musicInputField = document.getElementById('edit-music');
+  musicInputField.value = musicRatingOldVal;
+
+  //change edit section border color and submit button 
+  let trackerEditDiv = document.getElementById('tracker-edit');
+  trackerEditDiv.style.border = "7px solid rgba(218, 226, 98, 0.99)";
+
+  let trackerEditButton = document.getElementById('edit-submit')
+  trackerEditButton.style.background = "rgba(218, 226, 98, 0.99)"
+
+  //store data
+  currentlyEditingOldData = {
+    name: name.textContent,
+    year: year.textContent,
+    plotRating: plotRatingOldVal,
+    actingRating: actingRatingOldVal,
+    musicRating: musicRatingOldVal
+  };
+
+}
+
+const clearEditing = function () {
+  //reset edit fields
+  let editName = document.getElementById('edit-name');
+  editName.textContent = 'Movie Name'
+
+  let editYear = document.getElementById('edit-year');
+  editYear.textContent = 'Movie Year'
+
+  let plotInputField = document.getElementById('edit-plot');
+  plotInputField.value = ''
+
+  let actingInputField = document.getElementById('edit-acting');
+  actingInputField.value = ''
+
+  let musicInputField = document.getElementById('edit-music');
+  musicInputField.value =''
+
+  //change edit section border color and submit button 
+  let trackerEditDiv = document.getElementById('tracker-edit');
+  trackerEditDiv.style.border = "7px solid rgba(22, 109, 45, 0.79)";
+
+  let trackerEditButton = document.getElementById('edit-submit')
+  trackerEditButton.style.background = "rgb(106, 144, 48)"
+
+  currentlyEditingOldData = null; 
+}
+
+const submitEdits = async function ( ) {
+
+  const name = document.getElementById( "edit-name" ),
+        year = document.getElementById('edit-year'),
+        plotRating = document.getElementById( "edit-plot" ),
+        actingRating = document.getElementById( "edit-acting" ),
+        musicRating = document.getElementById( "edit-music" ),
+        newData = { name: name.textContent, 
+          year: year.textContent, 
+          plotRating: plotRating.value, 
+          actingRating: actingRating.value, 
+          musicRating: musicRating.value }
+  
+  if (currentlyEditingOldData) {
+    json = { newData: newData, oldData: currentlyEditingOldData },
+    body = JSON.stringify( json )
+  }
+  else {
+    console.log("no previous data found")
+    return;
+  }
+        
+  const response = await fetch( "/", {
+    method:"PATCH",
+    body 
+  })
+
+  const data = await response.json()
+
+  console.log( "text:", data )
+
+  populateTable(data)
+
+  clearEditing()
+}
+      
+const seeResults = async function ( event ) {
+  event.preventDefault()
+
+  clearEditing()
+
+  const response = await fetch( "/results", {
+    method:'GET', 
+  })
+
+  const data = await response.json()
+
+  console.log( "text2:", data )
+
+  populateTable(data)
+  
+}
+
+const populateTable = function (data) {
+  const tableBody = document.getElementById("movie-table-body");
+
+  //clear the table
+  tableBody.innerHTML = '';
+
+  data.forEach((movie, index) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td id="name-${index}">${movie['name']}</td>
+      <td class="number-cell" id="year-${index}">${movie['year']}</td>
+      <td class="number-cell" id="plotRating-${index}">${movie['plotRating']}</td>
+      <td class="number-cell" id="actingRating-${index}">${movie['actingRating']}</td>
+      <td class="number-cell" id="musicRating-${index}">${movie['musicRating']}</td>
+      <td class="number-cell" id="overallRating-${index}">${movie['overallRating']}</td>
+      <td><button onclick="deleteRow(${index})">Delete</button></td>
+      <td><button onclick="editRow(${index})">Edit</button></td>
+    `;
+    tableBody.appendChild(row);
+  });
+
 }
 
 window.onload = function() {
-   const button = document.querySelector("button");
-  button.onclick = submit;
+  const form = document.querySelector("#inputForm");
+  const SeeResultsButton = document.querySelector("#results-request");
+  const SubmitEditsButton = document.getElementById('edit-submit')
+  const editCancelButton = document.getElementById('edit-cancel')
+
+  form.addEventListener("submit", submit); 
+  SeeResultsButton.addEventListener("click", seeResults);
+  SubmitEditsButton.addEventListener("click", submitEdits);
+  editCancelButton.addEventListener("click", clearEditing)
+  
 }
--- server.improved.js
+++ server.improved.js
@@ -1,17 +1,15 @@
 const http = require( "http" ),
       fs   = require( "fs" ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library if you"re testing this on your local machine.
-      // However, Glitch will install it automatically by looking in your package.json
-      // file.
       mime = require( "mime" ),
       dir  = "public/",
       port = 3000
 
+
+//store movie name, year, plot rating, acting rating, music rating, overall rating (derived)
 const appdata = [
-  { "model": "toyota", "year": 1999, "mpg": 23 },
-  { "model": "honda", "year": 2004, "mpg": 30 },
-  { "model": "ford", "year": 1987, "mpg": 14} 
+  { "name": "Knives Out", "year": 2019, "plotRating": 10, "actingRating": 10, "musicRating": 10, "overallRating": 10 },
+  { "name": "Superman", "year": 2025, "plotRating": 9, "actingRating": 9, "musicRating": 10, "overallRating": 9.33 },
+  { "name": "Little Women", "year": 2019, "plotRating": 10, "actingRating": 10, "musicRating": 9, "overallRating": 9.66 }
 ]
 
 const server = http.createServer( function( request,response ) {
@@ -19,6 +17,11 @@ const server = http.createServer( function( request,response ) {
     handleGet( request, response )    
   }else if( request.method === "POST" ){
     handlePost( request, response ) 
+  }else if ( request.method === "DELETE") {
+    handleDelete( request, response)
+  }
+  else if ( request.method === "PATCH") {
+    handlePatch( request, response)
   }
 })
 
@@ -27,7 +30,10 @@ const handleGet = function( request, response ) {
 
   if( request.url === "/" ) {
     sendFile( response, "public/index.html" )
-  }else{
+  }
+  else if ( request.url == "/results" ) {
+    sendData( response )
+  } else{
     sendFile( response, filename )
   }
 }
@@ -40,12 +46,109 @@ const handlePost = function( request, response ) {
   })
 
   request.on( "end", function() {
-    console.log( JSON.parse( dataString ) )
+    let dataJSON = JSON.parse( dataString )
 
-    // ... do something with the data here!!!
+    //convert inputs to the right type
+    dataJSON["year"] = parseInt(dataJSON["year"])
+    dataJSON["plotRating"] = parseInt(dataJSON["plotRating"])
+    dataJSON["actingRating"] = parseInt(dataJSON["actingRating"])
+    dataJSON["musicRating"] = parseInt(dataJSON["musicRating"])
 
-    response.writeHead( 200, "OK", {"Content-Type": "text/plain" })
-    response.end("test")
+    //calculate and add derived field
+    let newObj = addDerivedField(dataJSON)
+    
+    //add to appdata
+    appdata.push(newObj)
+    console.log("New data added to appdata:", newObj)
+
+    response.writeHeader( 200, {"Content-Type": "text" })
+    response.end(JSON.stringify(appdata))
+  })
+}
+
+const handleDelete = function ( request, response ) {
+  console.log("delete")
+
+   let dataString = ""
+
+  request.on( "data", function( data ) {
+      dataString += data 
+  })
+
+  request.on( "end", function() {
+    let dataJSON = JSON.parse( dataString )
+
+    //find data in appdata
+    const dataIndex = appdata.findIndex(movie =>
+      movie['name'] === dataJSON['name'] &&
+      movie['year'] === parseInt(dataJSON['year'])  &&
+      movie['plotRating'] === parseInt(dataJSON['plotRating'])  &&
+      movie['actingRating'] === parseInt(dataJSON['actingRating'])  &&
+      movie['musicRating'] === parseInt(dataJSON['musicRating']) 
+    );
+
+    console.log("looking to delete ", dataIndex, appdata[dataIndex])
+
+    //check if valid
+    if (dataIndex === -1 ) {
+      console.log("No movie entry found")
+
+      response.writeHeader( 404, {"Content-Type": "text" })
+      response.end(JSON.stringify(appdata))
+    }
+    else {
+      appdata.splice(dataIndex, 1)
+
+      response.writeHeader( 200, {"Content-Type": "text" })
+      response.end(JSON.stringify(appdata))
+    }
+  })
+}
+
+const handlePatch = function ( request, response ) {
+   let dataString = ""
+
+  request.on( "data", function( data ) {
+      dataString += data 
+  })
+
+  request.on( "end", function() {
+    let dataJSON = JSON.parse( dataString )
+
+    //find data in appdata
+    const dataIndex = appdata.findIndex(movie =>
+      movie['name'] === dataJSON["oldData"]['name'] &&
+      movie['year'] === parseInt(dataJSON["oldData"]['year'])  &&
+      movie['plotRating'] === parseInt(dataJSON["oldData"]['plotRating'])  &&
+      movie['actingRating'] === parseInt(dataJSON["oldData"]['actingRating'])  &&
+      movie['musicRating'] === parseInt(dataJSON["oldData"]['musicRating']) 
+    );
+
+    //check if valid
+    if (dataIndex === -1 ) {
+      console.log("No movie entry found")
+
+      response.writeHeader( 404, {"Content-Type": "text" })
+      response.end(JSON.stringify(appdata))
+    }
+    else {
+      //delete entry and add new edited version
+      appdata.splice(dataIndex, 1)
+
+      //convert inputs to the right type
+      dataJSON["newData"]["year"] = parseInt(dataJSON["newData"]["year"])
+      dataJSON["newData"]["plotRating"] = parseInt(dataJSON["newData"]["plotRating"])
+      dataJSON["newData"]["actingRating"] = parseInt(dataJSON["newData"]["actingRating"])
+      dataJSON["newData"]["musicRating"] = parseInt(dataJSON["newData"]["musicRating"])
+
+      let newObj = addDerivedField(dataJSON["newData"])
+
+      //add to appdata
+      appdata.push(newObj)
+
+      response.writeHeader( 200, {"Content-Type": "text" })
+      response.end(JSON.stringify(appdata))
+    }
   })
 }
 
@@ -69,6 +172,25 @@ const sendFile = function( response, filename ) {
 
      }
    })
+}
+
+const sendData = function( response) {
+  response.writeHeader( 200, { "Content-Type": 'text' })
+  response.end(JSON.stringify(appdata))
+}
+
+const addDerivedField = function( dataJSON ) {
+  let total = dataJSON["plotRating"] + dataJSON["actingRating"] + dataJSON["musicRating"]
+
+  let newObject = { 
+      "name": dataJSON["name"], 
+      "year": dataJSON["year"], 
+      "plotRating": dataJSON["plotRating"], 
+      "actingRating": dataJSON["actingRating"], 
+      "musicRating": dataJSON["musicRating"], 
+      "overallRating": Math.round((total / 3) * 100) / 100}
+
+  return newObject
 }
 
 server.listen( process.env.PORT || port )
